9cd83f9695505ef0840f73ccc3c2dfcf4c245079
Prakash Bhagat <prakashbhagat2006@gmail.com>
2026-06-29 18:35:45 +0200

    Initial Commit

---
+++ CRUNCH/CRUNCHApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUNCHApp.swift
+//  CRUNCH
+//
+//  Created by Prakash on 29/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CRUNCHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUNCHTests/CRUNCHTests.swift
@@ -0,0 +1,18 @@
+//
+//  CRUNCHTests.swift
+//  CRUNCHTests
+//
+//  Created by Prakash on 29/06/2026.
+//
+
+import Testing
+
+struct CRUNCHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
